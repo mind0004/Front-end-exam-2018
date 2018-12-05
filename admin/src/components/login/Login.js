66277c4011da5425bfe5fa802212763095ae953d
Lars <you@example.com>
2018-12-05 22:17:07 +0100

    Login styling done

--- admin/src/components/login/Login.js
+++ admin/src/components/login/Login.js
@@ -1,39 +1,43 @@
 import React, { Component } from "react";
+import Background from "../../assets/img/background-line-grey-half.svg";
 
 class Login extends Component {
   render() {
     return (
       <div id="login">
+        <div className="background">
+          <div>
+            <img src={Background} alt="background shape" />
+          </div>
+        </div>
+
         <div className="container">
           <h2>Admin Login</h2>
-
           <form>
             <div className="input-field">
-              <input type="text" name="email" placeholder=" " />{" "}
+              <input type="text" name="email" placeholder=" " />
               <label>E-mail</label>
               <div className="underline" />
             </div>
 
             <div className="input-field">
-              <input type="text" name="email" placeholder=" " />{" "}
+              <input type="text" name="email" placeholder=" " />
               <label>Password</label>
               <div className="underline" />
             </div>
 
-            <div>
+            <div className="forgot">
               <p>Forgot email/password?</p>
             </div>
 
-            <div className="submit-button">
-              <input type="submit" />
+            <div className="submit">
+              <input type="submit" value="Login" className="submit-button" />
             </div>
           </form>
+        </div>
 
-          <div>
-            <a href="../index.html" class="go-back">
-              &lt; return to Polyeco
-            </a>
-          </div>
+        <div className="return">
+          <a href="../index.html">&lt; return to Polyeco</a>
         </div>
       </div>
     );
