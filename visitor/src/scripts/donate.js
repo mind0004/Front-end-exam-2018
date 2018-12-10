efb6f7dd930a7a58fde160a84e07c8da200fc63e
Mindaugas Jukna <mindaugas9494@hotmail.com>
2018-12-10 11:28:58 +0100

    donation sending to firebase in progress

--- visitor/src/scripts/donate.js
+++ visitor/src/scripts/donate.js
@@ -23,3 +23,22 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
   output.innerHTML = this.value;
 };
+
+// Save data to sessionStorage
+sessionStorage.setItem("key", "value");
+const step1Button = document.querySelector("#step-1 .step1Button");
+
+step1Button.addEventListener("click", proceedToStep2);
+
+function proceedToStep2() {
+  const donationInput = document.querySelector("#step-1 .slider");
+}
+
+// Get saved data from sessionStorage
+var data = sessionStorage.getItem("key");
+
+// Remove saved data from sessionStorage
+sessionStorage.removeItem("key");
+
+// Remove all saved data from sessionStorage
+sessionStorage.clear();
