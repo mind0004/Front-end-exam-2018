c181b1e9ef4016e699eb8693f12c70a65ecc881b
Lars <you@example.com>
2018-12-20 11:34:27 +0100

    Live data step 1, implemented input text in slider, fixed research textbox text dissapears

--- visitor/src/scripts/donate-1.js
+++ visitor/src/scripts/donate-1.js
@@ -17,22 +17,6 @@ db.settings({
   timestampsInSnapshots: true
 });
 
-//Get overview of latets three donations and top three donators
-function getOverview() {
-  db.collection("overview_visitor")
-    .get()
-    .then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        console.log("Your document data is: ", doc.data());
-      });
-    })
-    .catch(function(error) {
-      console.log("Error getting documents: ", error);
-    });
-}
-getOverview();
-
 //Mini
 
 /* ==========================================================================
@@ -46,18 +30,43 @@ const body = document.querySelector("body");
 document.addEventListener("DOMContentLoaded", init);
 function init() {
   //do stuff after page has loaded
+  getOverview(); //get latest donors and top donors
 }
 
 /* ==========================================================================
       Functions
       ========================================================================== */
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
+const slider = document.getElementById("myRange");
+const sliderInput = document.querySelector(".slidecontainer .amount input");
+const sliderInputLine = document.querySelector(".slidecontainer .amount .line");
+let allowStep2 = true;
 
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  console.log(this.value);
+  sliderInput.value = this.value;
 };
+
+sliderInput.addEventListener("input", checkInput);
+
+//Check input for number and handle if not
+function checkInput(e) {
+  console.log(e.target.value);
+  //check if value is a valid number (even number as strings)
+  if (isNaN(e.target.value)) {
+    //is not a number
+    console.log("Not a number");
+    allowStep2 = false;
+    sliderInputLine.style.background = "red";
+    step1Button.style.background = "#acacac";
+  } else {
+    //is a number
+    console.log("Nummberr");
+    allowStep2 = true;
+    slider.value = e.target.value;
+    sliderInputLine.style.removeProperty("background");
+    step1Button.style.removeProperty("background");
+  }
+}
 
 //Change monthly/yearly
 let dropdownOpen = false;
@@ -65,6 +74,7 @@ let currentPeriod = "monthly";
 const dropdown = document.querySelector("#step-1 .dropdown");
 let dropdownLIS;
 const dropdownUL = document.querySelector("#step-1 .dropdown ul");
+const dropdownLine = document.querySelector("#step-1 .dropdown .line");
 
 //Open/close period dropdown
 dropdown.addEventListener("click", toggleDropdown);
@@ -73,9 +83,11 @@ function toggleDropdown() {
   if (dropdownOpen) {
     //close dropdown
     dropdown.style.removeProperty("height");
+    dropdownLine.style.removeProperty("width");
   } else {
     //open dropdown
-    dropdown.style.height = "42px";
+    dropdown.style.height = "60px";
+    dropdownLine.style.width = "0px";
   }
   dropdownOpen = !dropdownOpen;
 }
@@ -107,7 +119,12 @@ const step1Button = document.querySelector("#step-1 .step1Button");
 
 step1Button.addEventListener("click", proceedToStep2);
 
-function proceedToStep2() {
+function proceedToStep2(e) {
+  if (!allowStep2) {
+    //if donation amount is not a number, dont allow to continue
+    e.preventDefault();
+  }
+
   const donationInput = document.querySelector("#step-1 .slider");
   const checkBox = document.querySelector("#step-1 #checkbox");
   const type = checkBox.checked ? currentPeriod : "one-time";
@@ -120,12 +137,50 @@ function proceedToStep2() {
 
   sessionStorage.setItem("step1Data", JSON.stringify(step1Data));
 }
+let overview = {};
+const recentDonorsTemplate = document.querySelector("#recent-donors-template")
+  .content;
+const topDonorsTemplate = document.querySelector("#top-donors-template")
+  .content;
 
-/*
-// Get saved data from sessionStorage
-var data = sessionStorage.getItem("key");
+//Get overview of latets three donations and top three donators
+function getOverview() {
+  db.collection("overview_visitor")
+    .get()
+    .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log("Your document data is: ", doc.data());
+        overview = doc.data();
+        displayRecentDonors();
+        displayTopDonors();
+      });
+    })
+    .catch(function(error) {
+      console.log("Error getting documents: ", error);
+    });
+}
 
-// Remove saved data from sessionStorage
-sessionStorage.removeItem("key");
+function displayRecentDonors() {
+  const recentDonors = document.querySelector("#recentdonors-grid");
 
-*/
+  overview.latestDonations.forEach(elem => {
+    let clone = recentDonorsTemplate.cloneNode(true);
+    clone.querySelector(".card-content p").textContent = elem.name;
+    clone.querySelector(".card-content2 p").textContent =
+      elem.amount + overview.currency;
+    recentDonors.appendChild(clone);
+  });
+}
+
+function displayTopDonors() {
+  const topDonors = document.querySelector("#topdonors-grid");
+
+  overview.topDonors.forEach(elem => {
+    let clone = topDonorsTemplate.cloneNode(true);
+    clone.querySelector(".card-content p").textContent = elem.name;
+    clone.querySelector(".card-content2 p").textContent =
+      elem.amount + overview.currency;
+    topDonors.appendChild(clone);
+  });
+}
